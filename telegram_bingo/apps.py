8490dcb459aafa8f7193fb45992561439cbcab3b
seud <seudsahm1@gmail.com>
2025-06-05 23:36:21 +0300

    mad to use webhook

--- telegram_bingo/apps.py
+++ telegram_bingo/apps.py
@@ -1,17 +1,6 @@
-# telegram_bingo/apps.py
 from django.apps import AppConfig
-import asyncio
-from .views import application
+
 
 class TelegramBingoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'telegram_bingo'
-
-    def ready(self):
-        # Start Telegram bot async
-        asyncio.ensure_future(start_bot())
-
-async def start_bot():
-    await application.initialize()
-    await application.start()
-    await application.updater.start()
--- telegram_bingo/views.py
+++ telegram_bingo/views.py
@@ -1,25 +1,47 @@
-# telegram_bingo/views.py
-
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from telegram import Update
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler
 import os 
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
+# Get bot token
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# Initialize your bot application (ensure this is done only once)
+# Import command functions
+from Bot.commands import (
+    start_command,
+    join_command,
+    card_command,
+    create_command,
+    mark_command
+)
+
+# Initialize bot application
 application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
+# Register bot commands
+application.add_handler(CommandHandler("start", start_command))
+application.add_handler(CommandHandler("create", create_command))
+application.add_handler(CommandHandler("join", join_command))
+application.add_handler(CommandHandler("card", card_command))
+application.add_handler(CommandHandler("mark", mark_command))
+
+# Webhook view to receive Telegram updates
 @csrf_exempt
 def telegram_webhook(request):
     if request.method == 'POST':
-        print("Webhook received")
+        # Parse the update sent by Telegram
         update = Update.de_json(json.loads(request.body), application.bot)
+        
+        # Put the update in the queue to be handled
         application.update_queue.put_nowait(update)
+        
         return JsonResponse({'status': 'ok'})
+    
+    # If not a POST request, return 405 error
     return JsonResponse({'status': 'method not allowed'}, status=405)
