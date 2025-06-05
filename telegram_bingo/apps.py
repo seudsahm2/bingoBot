6a5adb4a0f06432f03e07caf5c190235cc1b3c05
seud <seudsahm1@gmail.com>
2025-06-06 00:56:01 +0300

    import os added

--- telegram_bingo/apps.py
+++ telegram_bingo/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 import asyncio
-
+import os
 class TelegramBingoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'telegram_bingo'
